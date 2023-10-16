2fae0c870232ca4d4449aedca3a31813188324f6
abdullahafouaadus <abdullahafouaad.us@gmail.com>
2023-10-16 19:30:28 +0300

    again

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,12 @@
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+	int i = 0;
+	char c[] = "_putchar\n";
+		while (c[i] != '\0')
+	{
+		_putchar(c[i]);
+		i++;
+	}
 		return (0);
 }

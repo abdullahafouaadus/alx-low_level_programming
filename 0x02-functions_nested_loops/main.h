7009cd8715dc5380425bd5af12b2c3fcb074d266
abdullahafouaadus <abdullahafouaad.us@gmail.com>
2023-10-16 18:41:09 +0300

    the first

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,6 +7,9 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
+char c[] = "_putchar\n";
+
+int i = 0;
 int _putchar(char c)
 {
 	return (write(1, &c, 1));

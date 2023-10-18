2b504757e292ff3b6a3e95c10451bce481d91d61
abdullahafouaadus <abdullahafouaad.us@gmail.com>
2023-10-18 04:53:04 +0300

    alphapet

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -17,6 +16,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
 
 #endif

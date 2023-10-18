e7db2eb3152378726d80c2d18928aecc6f2c3aeb
abdullahafouaadus <abdullahafouaad.us@gmail.com>
2023-10-18 05:23:56 +0300

    let's see

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,3 @@
-#include "main.h"
-/**
- * main - Entry point
- *
- * Description: print all aplhabet letters
- *
- * Return: Always 0 (Success)
-*/
-void print_alphabet(void);
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
-
-
 void print_alphabet(void)
 {
 	char ch = 'a';

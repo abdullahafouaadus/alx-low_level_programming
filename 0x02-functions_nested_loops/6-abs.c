335af2498cb0a980c46d9036fa1fa0ccb9eb7c69
abdullahafouaadus <abdullahafouaad.us@gmail.com>
2023-10-22 03:28:55 +0300

    really

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * _abs - function that computes the absolute
- *       value of an integer
- *
- * @n: takes in integer type input for function
- *
- * Return: Always 0 (Success)
+* _abs - function that computes the absolute
+*       value of an integer
+*r = shfsdf
+* @n: takes in integer type input for function
+*
+* Return: Always 0 (Success)
 */
 
 
-int _abs(int r)
+int _abs(int n)
 {
-    return abs(r);
+	return (abs(n));
 }
